a1e9bcc18790dfbf80b49c3908b25fb4145be73c
oyvindsabo <oyvind.sabo@gmail.com>
2019-09-30 15:55:29 +0200

    Add OrbitControls

--- front-end/src/components/spaceFrameVisualization/SpaceFrameVisualization.tsx
+++ front-end/src/components/spaceFrameVisualization/SpaceFrameVisualization.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import * as THREE from 'three';
 import { SpaceFrameData } from '../../types';
 import { Vector3 } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 interface TrussVisualizationProps {
   spaceFrameData: SpaceFrameData;
@@ -17,7 +18,36 @@ class TrussVisualization extends Component<TrussVisualizationProps> {
       0.1,
       1000
     );
+    camera.position.set(0, 0, 50);
+
+    //Create a WebGLRenderer and turn on shadows in the renderer
     var renderer = new THREE.WebGLRenderer();
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+
+    renderer.gammaInput = true;
+    renderer.gammaOutput = true;
+
+    var controls = new OrbitControls(camera, renderer.domElement);
+    controls.addEventListener('change', this.render);
+    controls.minDistance = 50;
+    controls.maxDistance = 500;
+    controls.enablePan = false;
+
+    var ambient = new THREE.AmbientLight(0xffffff, 0.1);
+    scene.add(ambient);
+
+    //Create a SpotLight and turn on shadows for the light
+    var light = new THREE.SpotLight(0xffffff);
+    light.castShadow = true; // default false
+    scene.add(light);
+
+    //Set up shadow properties for the light
+    light.shadow.mapSize.width = 512; // default
+    light.shadow.mapSize.height = 512; // default
+    light.shadow.camera.near = 0.5; // default
+    light.shadow.camera.far = 500; // default
+
     renderer.setSize(window.innerWidth, window.innerHeight);
     this.myRef.appendChild(renderer.domElement);
 
@@ -31,10 +61,25 @@ class TrussVisualization extends Component<TrussVisualizationProps> {
     const geometry = new THREE.TubeGeometry(helix, 128, 2.5, 32);
 
     //const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
+
+    cube.castShadow = true; //default is false
+    cube.receiveShadow = false; //default
+
     scene.add(cube);
-    camera.position.z = 50;
+
+    //Create a plane that receives shadows (but does not cast them)
+    var planeGeometry = new THREE.PlaneBufferGeometry(100, 100, 32, 32);
+    var planeMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.receiveShadow = true;
+    scene.add(plane);
+
+    //Create a helper for the shadow camera (optional)
+    var helper = new THREE.CameraHelper(light.shadow.camera);
+    scene.add(helper);
+
     const animate = () => {
       requestAnimationFrame(animate);
       cube.rotation.x += 0.01;
