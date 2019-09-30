73b6a5b2fef6636bb2523de88efdd0434dcfcade
oyvindsabo <oyvind.sabo@gmail.com>
2019-09-30 13:36:38 +0200

    Implement basic three.js example

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TrussVisualization from './components/trussVisualization/TrussVisualization';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <TrussVisualization />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,6 +23,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
